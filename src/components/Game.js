f5e4cc1a206947cb504551fe1cb7e647bad6da6d
Ivan <ivan1501@gmail.com>
2020-03-09 16:37:09 +0100

    Now you can put X and O into game. Need to create rules for winning

--- src/components/Game.js
+++ src/components/Game.js
@@ -13,6 +13,19 @@ export default class Game extends Component {
         }
     }
 
+    handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat({
+                squares: squares
+            }),
+            xIsNext: !this.state.xIsNext,
+            stepNumber: history.length
+        });
+    }
     render(){
         const history = this.state.history;
         const current = history[this.state.stepNumber];
