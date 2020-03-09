24f2a92007006c57c688ef14e587bf7e4e3272c7
Ivan <ivan1501@gmail.com>
2020-03-09 17:19:30 +0100

    Added option draw to the game.

--- src/components/Game.js
+++ src/components/Game.js
@@ -51,10 +51,12 @@ export default class Game extends Component {
             )
         });
         var status;
-        if (winner) {
-            status = 'Winner is ' + winner;
+        if(winner && winner !== 'draw'){
+            status = 'Winner: ' + winner;
+        } else if (winner && winner === 'draw'){
+            status = "It's a " + winner;
         } else {
-            status = 'Next Player is ' + (this.state.xIsNext ? 'X' : 'O');
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
 
         return (
@@ -74,22 +76,24 @@ export default class Game extends Component {
 
 function calculateWinner(squares) {
     const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
-            return squares[a];
-        }
+for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
     }
-
-    return null;
+    else if(!squares.includes(null)){
+        return 'draw';
+    }
+}
+return null;
 }
